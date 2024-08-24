9e5192028796c2c32f70e7080244c05a4f4280a2
unknown <angirekulagayatri651@gmail.com>
2024-08-24 13:13:50 +0530

    add all files

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    if (!name || !email || !message) {
+      alert('Please fill in all fields.');
+      event.preventDefault();
+    } else {
+      alert('Thank you for your message!');
+    }
+  });
+  
